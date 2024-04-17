4b700b92d9323e1eb3862930d0dd3bf4d85de967
hezijie <lonegunmanb@hotmail.com>
2024-04-17 16:34:34 +0800

    support define cty.Value directy in block, would add it directly into the eval context. support customized block stringfy

--- block_test.go
+++ block_test.go
@@ -1,6 +1,7 @@
 package golden
 
 import (
+	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/zclconf/go-cty/cty"
 	"math/big"
@@ -107,3 +108,111 @@ func Test_LocalBlocksValueShouldBeAFlattenObject(t *testing.T) {
 	assert.True(t, AreCtyValuesEqual(numberVal, values.GetAttr("number_value")))
 	assert.True(t, AreCtyValuesEqual(stringVal, values.GetAttr("string_value")))
 }
+
+func TestBlockToString(t *testing.T) {
+
+	cases := []struct {
+		desc     string
+		b        Block
+		expected string
+	}{
+		{
+			desc:     "normal_block_should_return_json_marshal",
+			b:        fakeBlock{Input: "input"},
+			expected: `{"input":"input"}`,
+		},
+		{
+			desc:     "block_implement_stringer_interface_should_return_customized_string",
+			b:        customizedStringBlock{fakeBlock{Input: "input"}},
+			expected: `input`,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.desc, func(t *testing.T) {
+			actual := BlockToString(c.b)
+			assert.Equal(t, c.expected, actual)
+		})
+	}
+}
+
+type fakeBlock struct {
+	Input string `json:"input"`
+}
+
+type customizedStringBlock struct {
+	fakeBlock
+}
+
+func (c customizedStringBlock) String() string {
+	return c.Input
+}
+
+func (c fakeBlock) Id() string {
+	panic("implement me")
+}
+
+func (c fakeBlock) Name() string {
+	panic("implement me")
+}
+
+func (c fakeBlock) Type() string {
+	panic("implement me")
+}
+
+func (c fakeBlock) BlockType() string {
+	panic("implement me")
+}
+
+func (c fakeBlock) Address() string {
+	panic("implement me")
+}
+
+func (c fakeBlock) HclBlock() *HclBlock {
+	panic("implement me")
+}
+
+func (c fakeBlock) EvalContext() *hcl.EvalContext {
+	panic("implement me")
+}
+
+func (c fakeBlock) BaseValues() map[string]cty.Value {
+	panic("implement me")
+}
+
+func (c fakeBlock) PreConditionCheck(context *hcl.EvalContext) ([]PreCondition, error) {
+	panic("implement me")
+}
+
+func (c fakeBlock) AddressLength() int {
+	panic("implement me")
+}
+
+func (c fakeBlock) CanExecutePrePlan() bool {
+	panic("implement me")
+}
+
+func (c fakeBlock) getDownstreams() []Block {
+	panic("implement me")
+}
+
+func (c fakeBlock) getForEach() *ForEach {
+	panic("implement me")
+}
+
+func (c fakeBlock) markExpanded() {
+	panic("implement me")
+}
+
+func (c fakeBlock) isReadyForRead() bool {
+	panic("implement me")
+}
+
+func (c fakeBlock) markReady() {
+	panic("implement me")
+}
+
+func (c fakeBlock) expandable() bool {
+	panic("implement me")
+}
+
+var _ Block = fakeBlock{}
