2a06348a17256e9feb0ad9ad3f405fad8ee23d36
zjhe <lonegunmanb@hotmail.com>
2024-05-11 18:19:28 +0800

    prepare for variable block, wip

---
+++ variable.go
@@ -0,0 +1,80 @@
+package golden
+
+import (
+	"fmt"
+	"github.com/hashicorp/hcl/v2/ext/typeexpr"
+	"github.com/zclconf/go-cty/cty"
+	"os"
+)
+
+var DslFullName string
+var DslAbbreviation string
+
+var _ Variable = &VariableBlock{}
+var _ PrePlanBlock = &VariableBlock{}
+var _ PlanBlock = &VariableBlock{}
+
+type Variable interface {
+	SingleValueBlock
+	Variable()
+}
+
+type VariableBlock struct {
+	*BaseBlock
+	VariableType  *cty.Type
+	VariableValue cty.Value
+}
+
+func (v *VariableBlock) ExecuteDuringPlan() error {
+	return nil
+}
+
+func (v *VariableBlock) Type() string {
+	return ""
+}
+
+func (v *VariableBlock) BlockType() string {
+	return "variable"
+}
+
+func (v *VariableBlock) AddressLength() int {
+	return 2
+}
+
+func (v *VariableBlock) CanExecutePrePlan() bool {
+	return true
+}
+
+func (v *VariableBlock) Value() cty.Value {
+	return v.VariableValue
+}
+
+func (v *VariableBlock) Variable() {}
+
+func (v *VariableBlock) ExecuteBeforePlan() error {
+	if err := v.ParseVariableType(); err != nil {
+		return err
+	}
+	panic("implement me")
+}
+
+func (v *VariableBlock) ParseVariableType() error {
+	typeAttr, ok := v.HclBlock().Body.Attributes["type"]
+	if !ok {
+		return nil
+	}
+	t, diag := typeexpr.Type(typeAttr.Expr)
+	if diag.HasErrors() {
+		return diag
+	}
+	v.VariableType = &t
+	return nil
+}
+
+func (v *VariableBlock) ReadValueFromEnv() *cty.Value {
+	env := os.Getenv(fmt.Sprintf("%s_VAR_%s", DslAbbreviation, v.name))
+	if env == "" {
+		return nil
+	}
+	panic("implement me")
+}
